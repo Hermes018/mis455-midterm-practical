c71708d3dcfb60118f499a06032052bef41f6cef
Ayman <aymanrahman30@gmail.com>
2026-07-04 06:27:05 +0600

    Implement interactive JS buttons with onclick event handlers

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// script.js - Phase 1
+// script.js
+
+function exploreProducts() {
+    alert("Navigating to our products gallery!");
+    window.location.href = "products.html";
+}
+
+function submitContact() {
+    alert("Thank you for your message! We will get back to you shortly.");
+}
